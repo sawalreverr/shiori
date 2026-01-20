8665756da861a990db214e9bbd6f7a92a5cd861c
Muhammad Shahwal <sawalrever23@gmail.com>
2026-01-20 18:51:34 +0700

    feat: initialize astro + svelte

---
+++ web/astro.config.mjs
@@ -0,0 +1,19 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import svelte from '@astrojs/svelte';
+import tailwindcss from '@tailwindcss/vite';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [svelte()],
+
+  vite: {
+    server: {
+      proxy: {
+        '/api': 'http://localhost:8080'
+      }
+    },
+    plugins: [tailwindcss()]
+  }
+});
